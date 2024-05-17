1423dc89d8781688e5a6304f87f5548e14195902
braundz <144893923+braundz@users.noreply.github.com>
2024-05-17 09:52:47 +0300

    Add files via upload

---
+++ JavaScript/yypppp.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var linkDiv = document.getElementById("linkDiv");
+    linkDiv.addEventListener("click", function() {
+        window.location.href = "pokupka.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    var linkDiv1 = document.getElementById("linkDiv1");
+    linkDiv1.addEventListener("click", function() {
+        window.location.href = "pokupka1.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    var linkDiv2 = document.getElementById("linkDiv2");
+    linkDiv2.addEventListener("click", function() {
+        window.location.href = "pokupka2.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    var linkDiv3 = document.getElementById("linkDiv3");
+    linkDiv3.addEventListener("click", function() {
+        window.location.href = "pokupka3.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    var linkDiv4 = document.getElementById("linkDiv4");
+    linkDiv4.addEventListener("click", function() {
+        window.location.href = "pokupka4.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    var linkDiv5 = document.getElementById("linkDiv5");
+    linkDiv5.addEventListener("click", function() {
+        window.location.href = "pokupka5.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    var linkDiv6 = document.getElementById("linkDiv6");
+    linkDiv6.addEventListener("click", function() {
+        window.location.href = "pokupka6.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    var linkDiv7 = document.getElementById("linkDiv7");
+    linkDiv7.addEventListener("click", function() {
+        window.location.href = "pokupka7.html";
+    });
+});
+
+          
